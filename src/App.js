91d827c9add8c0af5b00c60cdd8695ef1109ffeb
Anson Poon <ansonpoon77@gmail.com>
2024-05-08 01:48:50 -0700

    Added gradient background

--- src/App.js
+++ src/App.js
@@ -16,13 +16,21 @@ export default function App() {
 
   return (
     <main className={`body-font ${darkMode ? "dark" : "light"}`}>
-      <div className="container mx-auto max-w-8xl" >
+      <div
+        className={`w-full h-full bg-gradient-to-r ${
+          darkMode
+            ? "from-pink-400 via-blue-400 to-green-400"
+            : "from-pink-200 via-blue-200 to-green-200"
+        } background-animate`}
+      >
+      <div className="container mx-auto max-w-8xl">
         <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-        <About />
+        <About darkMode={darkMode} />
         <Projects />
         <Skills />
         <Resume />
         <Contact />
+      </div>
       </div>
     </main>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function About() {
+export default function About({ darkMode }) {
   return (
     <section id="about">
-      <div className="container mx-auto flex py-32 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow flex flex-col items-center">
+      <div className="container mx-auto flex md:flex-row flex-col items-center py-20">
+        <div className={`lg:flex-grow flex flex-col items-center px-20 py-20 rounded-3xl drop-shadow-2xl ${darkMode ? "bg-slate-400" : "bg-slate-200"}`}>
           <h1 className="title-font sm:text-5xl text-3xl mb-4 font-semibold">
             Anson Poon
           </h1>
